473eaee3b281e74371b2a4f76d0fe0a21cc68abf
Grigory Vlasov <54944844+GrigoryVV@users.noreply.github.com>
2020-04-20 15:28:19 +0700

    add changes to test pre-commit

--- packages/web/auth.module/src/index.ts
+++ packages/web/auth.module/src/index.ts
@@ -1,10 +1,10 @@
-export { getAuthModule } from "./auth.module";
-export { SignInStatusEnum, SignUpStatusEnum } from "./auth.contracts";
+export { getAuthModule } from './auth.module';
+export { SignInStatusEnum, SignUpStatusEnum } from './auth.contracts';
 
-import * as signInSelectors from "./signin/signin.selectors";
-import { signInActions } from "./signin/signin.actions";
+import * as signInSelectors from './signin/signin.selectors';
+import { signInActions } from './signin/signin.actions';
 
-import * as signUpSelectors from "./signup/signup.selectors";
-import { signUpActions } from "./signup/signup.actions";
+import * as signUpSelectors from './signup/signup.selectors';
+import { signUpActions } from './signup/signup.actions';
 
 export { signInSelectors, signInActions, signUpSelectors, signUpActions };
